388da409f2fefff007f97af0554c3f209a1d9d7b
Haxu <nguyenngocminh190901@gmail.com>
2022-06-19 17:26:58 +0700

    header tiktok UI 2

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,23 @@
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import classNames from "classnames/bind";
+import styles from './AccountItem.module.scss';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+  return (  
+    <div className={cx('wrapper')}>
+      <img className={cx('avatar')} src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/07b387e851ae1ee324d170145cddd1d2~c5_100x100.jpeg?x-expires=1655805600&x-signature=nrEw6ICFpMRQq987AFWhtIT0LOE%3D" alt="Hoaa" />
+      <div className={cx('info')}>
+        <p className={cx('name')}>
+          <span>Nguyen Van A</span>
+          <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+        </p>
+        <span className={cx('username')}>nguyenvana</span>
+      </div>
+    </div>
+  );
+}
+
+export default AccountItem;
