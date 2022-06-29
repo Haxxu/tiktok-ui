fcb9d1b3045f440b0a17e53657d257614d5ec1ca
Haxu <nguyenngocminh190901@gmail.com>
2022-06-29 08:08:23 +0700

    Áp dụng PropTypes vào dự án

--- src/components/Popper/Menu/Header.js
+++ src/components/Popper/Menu/Header.js
@@ -1,20 +1,26 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleLeft } from '@fortawesome/free-solid-svg-icons';
-import classNames from "classnames/bind";
+import classNames from 'classnames/bind';
+
 import styles from './Menu.module.scss';
 
 const cx = classNames.bind(styles);
 
 function Header({ title, onBack }) {
-
-	return (  
-    <header className={cx('header')}>
-      <button className={cx('back-btn')} onClick={onBack}>
-        <FontAwesomeIcon icon={faAngleLeft} />
-      </button>
-      <h4 className={cx('header-title')}>{title}</h4>
-    </header>
-	);
+    return (
+        <header className={cx('header')}>
+            <button className={cx('back-btn')} onClick={onBack}>
+                <FontAwesomeIcon icon={faAngleLeft} />
+            </button>
+            <h4 className={cx('header-title')}>{title}</h4>
+        </header>
+    );
 }
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+    onBack: PropTypes.func.isRequired,
+};
 
 export default Header;
--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import Tippy from '@tippyjs/react/headless';
 import { useState } from 'react';
@@ -65,5 +66,12 @@ function Menu({ children, items = [], hideOnClick = false, onChange = defaultFun
         </Tippy>
     );
 }
+
+Menu.propTypes = {
+    children: PropTypes.node.isRequired,
+    items: PropTypes.array,
+    hideOnClick: PropTypes.bool,
+    onChange: PropTypes.func,
+};
 
 export default Menu;
